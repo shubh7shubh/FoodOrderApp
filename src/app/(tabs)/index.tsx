f96d13ed4f599fcf79c602f8fb18d1d7a10300dd
shubham <khobragade.shubham@gmail.com>
2024-02-12 15:10:35 +0530

    fixed some commit error

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '@/src/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hellow worlddsfdsf</Text>
+      <Text style={styles.title}>Hellow wofdfsdrlddsfdsf</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
